82f8a68c7998bcf970cd4695ebcc0748c336895e
Hiura <hiura0012@gmail.com>
2025-12-03 12:24:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     description: 'Integrated API solution for your modern application development needs. Fast, secure, and reliable access.',
     icon: '/image/icon.png',
-    author: 'Rynn',
+    author: 'Hiura',
     info_url: 'https://whatsapp.com/channel/0029Vb9MIdiDDmFXasVPVG1t',
     links: [
         {
